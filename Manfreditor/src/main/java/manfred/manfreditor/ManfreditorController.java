b863a0b7ab11d50994817c43da95b1735c9ba9ca
Fabian Engelniederhammer <Fabian.Engelniederhammer@tngtech.com>
2020-07-02 21:31:23 +0200

    create separate module and make it run

---
+++ Manfreditor/src/main/java/manfred/manfreditor/ManfreditorController.java
@@ -0,0 +1,12 @@
+package manfred.manfreditor;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+
+public class ManfreditorController {
+    public Button button;
+
+    public void onClick(ActionEvent actionEvent) {
+        System.out.println("Hallo");
+    }
+}
