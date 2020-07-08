88fe751d271cec442b64f28c6cdb0ff564edec84
Fabian Engelniederhammer <Fabian.Engelniederhammer@tngtech.com>
2020-07-08 22:47:16 +0200

    invert accessability of map tiles upon clicking on them

--- Manfreditor/src/main/java/manfred/manfreditor/Controller.java
+++ Manfreditor/src/main/java/manfred/manfreditor/Controller.java
@@ -6,11 +6,13 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextInputDialog;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import manfred.manfreditor.exception.InvalidInputException;
 import manfred.manfreditor.map.Map;
+import manfred.manfreditor.map.MapPane;
 import manfred.manfreditor.map.MapReader;
 import net.rgielen.fxweaver.core.FxmlView;
 import org.springframework.stereotype.Component;
@@ -21,13 +23,15 @@ import java.util.Optional;
 @Component
 @FxmlView("manfreditor.fxml")
 public class Controller {
-    public static final int PIXEL_BLOCK_SIZE = 50;
+    private static final Color notAccessibleColor = new Color(1, 0, 0, 0.4);
+    private static final Color accessibleColor = new Color(0, 0, 0, 0.0);
 
     public Button loadMapButton;
-    public GridPane mapPane;
+    public MapPane mapPane;
     public ScrollPane mapPaneContainer;
 
     private final MapReader mapReader;
+    private Map map;
 
     public Controller(MapReader mapReader) {
         this.mapReader = mapReader;
@@ -45,27 +49,35 @@ public class Controller {
 
     private void loadMap(String mapName) {
         try {
-            Map map = mapReader.load(mapName);
+            map = mapReader.load(mapName);
 
             int horizontalSize = map.getArray().length;
             int verticalSize = map.getArray()[0].length;
-
-            clearMapPane();
-            mapPane.setMinWidth(horizontalSize * PIXEL_BLOCK_SIZE);
-            mapPane.setMinHeight(verticalSize * PIXEL_BLOCK_SIZE);
+            mapPane.resetWithSize(horizontalSize, verticalSize);
 
             for (int x = 0; x < horizontalSize; x++) {
                 for (int y = 0; y < verticalSize; y++) {
-                    if (!map.isAccessible(x, y)) {
-                        Rectangle rectangle = new Rectangle(x * 50, y * 50, 50, 50);
-                        rectangle.setFill(new Color(1, 0, 0, 0.4));
-                        mapPane.add(rectangle, x, y);
-                    }
+                    mapPane.addTile(
+                        createMapTile(map.getArray()[x][y]),
+                        x,
+                        y
+                    );
                 }
             }
         } catch (InvalidInputException | IOException e) {
             showErrorMessagePopup(e.getMessage());
         }
+    }
+
+    private Rectangle createMapTile(boolean isAccessible) {
+        Rectangle rectangle = new Rectangle(MapPane.PIXEL_BLOCK_SIZE, MapPane.PIXEL_BLOCK_SIZE);
+        rectangle.setFill(
+            isAccessible
+                ? accessibleColor
+                : notAccessibleColor
+        );
+        rectangle.setOnMouseClicked(this::onGridPaneClicked);
+        return rectangle;
     }
 
     private void showErrorMessagePopup(String message) {
@@ -76,9 +88,16 @@ public class Controller {
         alert.showAndWait();
     }
 
-    private void clearMapPane() {
-        Node gridLines = mapPane.getChildren().get(0);
-        mapPane.getChildren().clear();
-        mapPane.getChildren().add(0, gridLines);
+    public void onGridPaneClicked(MouseEvent mouseEvent) {
+        Node source = (Node) mouseEvent.getSource();
+        int x = GridPane.getColumnIndex(source);
+        int y = GridPane.getRowIndex(source);
+
+        map.getArray()[x][y] = !map.getArray()[x][y];
+        mapPane.getTile(x, y).setFill(
+            map.getArray()[x][y]
+                ? accessibleColor
+                : notAccessibleColor
+        );
     }
 }
